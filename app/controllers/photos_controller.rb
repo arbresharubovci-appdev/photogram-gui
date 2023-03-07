3d91deb0076367a88aeb1a16222fd304e5a36fd7
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-03-07 18:24:01 +0000

    photo details almost done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,4 +5,13 @@ class PhotosController < ApplicationController
     @list_of_photos = matching_photos.order({ :created_at => :desc })
     render({:template => "photo_templates/index.html.erb"})
   end
+
+def show_details
+  #Parameters: {"details_photos"=>"777"}
+  url_id = params.fetch("details_photos")
+  matching_photos = Photo.where({ :id => url_id })
+  @the_photo = matching_photos.at(0)
+  render({ :template => "photo_templates/show_details.html.erb"})
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 get("/users" , {:controller => "users", :action =>"index"})
 get("/users/:profile" , { :controller => "users" , :action => "show"})
 get("/photos" , { :controller => "photos" , :action => "index"})
+get("/photos/:details_photos", { :controller => "photos" , :action => "show_details"})
 
 end
