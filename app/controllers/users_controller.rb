38dfa70cf960d8d83d97c1dbd43769370dcfacd0
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-03-09 20:25:19 +0000

    add user name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,36 @@
 class UsersController < ApplicationController
   def index
     matching_users = User.all
-    @list_of_users = matching_users.order({ :username => :asc})
-    render({:template => "user_templates/index.html.erb"})
+    @list_of_users = matching_users.order({ :username => :asc })
+    render({ :template => "user_templates/index.html.erb" })
   end
 
   def show
-    #Parameters: {"profile"=>"anisa"}
-    url_username = params.fetch("profile")
-    matching_users = User.where({ :username => url_username})
+    #params: {"username"=>"my-name"}
+    username = params.fetch("username")
+    matching_users = User.where({ :username => username })
     @the_user = matching_users.first
-    render({:template => "user_templates/show.html.erb"})
+
+    render({ :template => "user_templates/show.html.erb" })
   end
 
   def record
     #Parameters: {"input_username"=>"Arbresha"}
     input_username = params.fetch("input_username")
 
-    a_new_username = User.new
+    new_user = User.new
 
-    a_new_username.username = input_username
+    new_user.username = input_username
 
-    a_new_username.save
+    new_user.save
 
-    next_url ="/users/" + a_new_username.id.to_s
-    redirect_to(next_url)
-    
+    user_show = "/users/#{new_user.username}"
+    redirect_to(user_show)
+
     #render({:template => "user_templates/record.html.erb"})
   end
 
-  
-  def comment 
+  def comment
     #Parameters: {"input_photo_id"=>"777", "input_author_id"=>"122", "input_body"=>"so goo"}
     input_photo_id = params.fetch("input_photo_id")
     input_author_id = params.fetch("input_author_id")
@@ -44,9 +44,13 @@ class UsersController < ApplicationController
 
     a_new_comment.save
 
-    next_url ="/photos/" + input_photo_id..to_s
+    next_url = "/photos/" + input_photo_id.to_s
+    puts next_url
     redirect_to(next_url)
     #render({:template => "user_templates/comment.html.erb"})
   end
 
+  def update
+    render({:template => "user_templates/update.html.erb"})
+  end
 end
